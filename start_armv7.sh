8065bd9399f19811e45b6eef31711d547f26cb82
Igor Opaniuk <igor.opaniuk@gmail.com>
2020-04-05 00:13:07 +0300

    start_armv7: adjust script
    Add workaround to use local qemu build (f.e. from vanilla OP-TEE build).
    
    Signed-off-by: Igor Opaniuk <igor.opaniuk@gmail.com>

--- start_armv7.sh
+++ start_armv7.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 
 BUILD_DIR=${PWD}/../build
-ROOT_NATIVE=$BUILD_DIR/tmp/work/qemu_optee32-poky-linux-gnueabi/core-image-minimal/1.0-r0/recipe-sysroot-native/
+ROOT_NATIVE=$BUILD_DIR/tmp/work/qemu_optee32-poky-linux-gnueabi/core-image-minimal/1.0-r0/recipe-sysroot-native
 DEPLOY_DIR=$BUILD_DIR/tmp/deploy/images/qemu-optee32/
-SOC_TERM=$ROOT_NATIVE/usr/bin/soc_term
+#SOC_TERM=$ROOT_NATIVE/usr/bin/soc_term
+SOC_TERM=/home/xdev/zondax/reps/QEMU/soc_term/soc_term
+
+ln -s $DEPLOY_DIR/optee/tee-header_v2.bin $DEPLOY_DIR/bl32.bin
+ln -s $DEPLOY_DIR/optee/tee-pager_v2.bin $DEPLOY_DIR/bl32_extra1.bin
+ln -s $DEPLOY_DIR/optee/tee-pageable_v2.bin $DEPLOY_DIR/bl32_extra2.bin
+ln -s $DEPLOY_DIR/u-boot.bin $DEPLOY_DIR/bl33.bin
+ln -s $DEPLOY_DIR/core-image-minimal-qemu-optee32.cpio.gz $DEPLOY_DIR/rootfs.cpio.gz
 
 nc -z  127.0.0.1 54320 || \
 gnome-terminal -x ${SOC_TERM} 54320
@@ -13,6 +20,7 @@ gnome-terminal -x ${SOC_TERM} 54321
 
 sleep 2
 
+#cd $DEPLOY_DIR && /home/xdev/zondax/reps/QEMU/qemu/arm-softmmu/qemu-system-arm \
 cd $DEPLOY_DIR && $ROOT_NATIVE/usr/bin/qemu-system-arm \
 	-s -S \
 	-nographic \
